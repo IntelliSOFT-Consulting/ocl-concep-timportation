610ceea0131fa729cff4eebb44b66840a61d7547
Geoff Wasilwa <geoffreywanjala@gmail.com>
2020-07-20 21:32:28 +0300

    Fix upload errors
    Add quotes to field names
    Add 'owner', 'owner_type' and 'source' information to resources being uploaded
    Add 'Authorization' header

--- ocl_uploader.js
+++ ocl_uploader.js
@@ -1,9 +1,13 @@
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu('OCL')
-    .addItem('Upload to OCL', 'uploadToOCL')
+    .addItem('Upload to OCL', 'parseAndSendToOcl')
     .addToUi();
 }
+
+var owner = "moshonk";
+var ownerType = "User";
+var source = "carepay-ciel"
 
 var CONCEPT_ID = "concept_id";
 var CONCEPT_CLASS = "concept_class";
@@ -13,7 +17,7 @@ var DOSAGE_FORM = "dosage_form";
 var DOSAGE_STRENGTH = "dosage_strength";
 var CIEL_ID = "ciel_id";
 var ICD10_ID = "icd10_id";
-var SNOMED_CT_ID = "snomed_id";
+var SNOMED_ID = "snomed_id";
 
 var columnToProperty = {
   "CAREPAY Map": CONCEPT_ID,
@@ -24,24 +28,25 @@ var columnToProperty = {
   "Dosage Strength": DOSAGE_STRENGTH,
   "CIEL Concept ID": CIEL_ID,
   "ICD-10 Map": ICD10_ID,
-  "SNOMED Map": SNOMED_CT_ID
+  "SNOMED Map": SNOMED_ID
 };
 
 var OCL_API_ENDPOINT = "https://api.openconceptlab.org/";
-var ORIGIN_SOURCE_URL = "/users/moshonk/collections/carepay-ciel/concepts/";
+var ORIGIN_SOURCE_URL = "/users/moshonk/sources/carepay-ciel/concepts/";
 var CEIL_SOURCE_URL = "/orgs/CIEL/sources/CIEL/concepts/";
 var ICD10_SOURCE_URL = "/orgs/WHO/sources/ICD-10-WHO/concepts/";
-var SNOMED_CT_SOURCE_URL = "/orgs/IHTSDO/sources/SNOMED-CT/concepts/";
+var SNOMED_SOURCE_URL = "/orgs/CIEL/sources/SNOMED-MVP/";
 
 /**
  * Parses current sheet and performs a bulk export of concepts and mappings to ocl.
  * Assumptions:
- * - The user will click the 'Upload to OCL' button while viewing an uploadable sheet
+ * - The user will only click the 'Upload to OCL' button while viewing an uploadable sheet
  * - Only one name will be provided for concepts
+ * - All names are of type 'Fully Specified' so as to pass OpenMRS concept validation
  * - All mappings are of type 'Same As'
  * - All mappings are internal mappings
  */
-function uploadToOCL() {
+function parseAndSendToOcl() {
   var range = SpreadsheetApp.getActiveSheet().getDataRange();
   var height = range.getHeight();
   var width = range.getWidth();
@@ -95,57 +100,68 @@ function uploadToOCL() {
   var requestBody = '';
   for (var row = 2; row <= height; row++) {
     if (skipRow(row)) {
-      continue; // row is incomplete, skip
+      continue; // data in row is incomplete, skip
     }
     // concepts resource
     requestBody += '{ ';
-    requestBody += 'type: "Concept", '
-    requestBody += 'id: "' + range.getCell(row, propertyToPosition[CONCEPT_ID]).getValue() + '", ';
-    requestBody += 'concept_class: "' + range.getCell(row, propertyToPosition[CONCEPT_CLASS]).getValue() + '", ';
-    requestBody += 'datatype: "' + range.getCell(row, propertyToPosition[DATATYPE]).getValue() + '", ';
-    requestBody += 'names: [{';
-    requestBody += 'name: "' + range.getCell(row, propertyToPosition[CONCEPT_NAME]).getValue() + '", ';
-    requestBody += 'locale: "en" ';
-    requestBody += '}]';
+    requestBody += '"type": "Concept", '
+    requestBody += '"id": "' + range.getCell(row, propertyToPosition[CONCEPT_ID]).getValue() + '", ';
+    requestBody += '"concept_class": "' + range.getCell(row, propertyToPosition[CONCEPT_CLASS]).getValue() + '", ';
+    requestBody += '"datatype": "' + range.getCell(row, propertyToPosition[DATATYPE]).getValue() + '", ';
+    requestBody += '"names": [{ ';
+    requestBody += '"name": "' + range.getCell(row, propertyToPosition[CONCEPT_NAME]).getValue() + '", ';
+    requestBody += '"locale": "en", ';
+    requestBody += '"name_type": "Fully Specified" ';
+    requestBody += '}], ';
     if (range.getCell(row, propertyToPosition[DOSAGE_FORM]).getValue()) { // assume that if dosage form is given then dosage strength is also given
-      requestBody += ', extras: { ';
-      requestBody += 'dosage_form: "' + range.getCell(row, propertyToPosition[DOSAGE_FORM]).getValue() + '", ';
-      requestBody += 'dosage_strength: "' + range.getCell(row, propertyToPosition[DOSAGE_STRENGTH]).getValue() + '" ';
-      requestBody += '}';
+      requestBody += '"extras": { ';
+      requestBody += '"dosage_form": "' + range.getCell(row, propertyToPosition[DOSAGE_FORM]).getValue() + '", ';
+      requestBody += '"dosage_strength": "' + range.getCell(row, propertyToPosition[DOSAGE_STRENGTH]).getValue() + '" ';
+      requestBody += '}, ';
     }
+    requestBody += '"owner": "' + owner +'", ';
+    requestBody += '"owner_type": "' + ownerType +'", ';
+    requestBody += '"source": "' + source +'" ';
     requestBody += '}\n';
 
     // mappings resource
     if (range.getCell(row, propertyToPosition[CIEL_ID]).getValue()) {
       // CEIL
       requestBody += '{ ';
-      requestBody += 'type: "Mapping", ';
-      requestBody += 'map_type: "Same As", ';
-      requestBody += 'from_concept_url: "' + ORIGIN_SOURCE_URL + range.getCell(row, propertyToPosition[CONCEPT_ID]).getValue() + '/", ';
-      requestBody += 'to_concept_url: "' + CEIL_SOURCE_URL + range.getCell(row, propertyToPosition[CIEL_ID]).getValue() + '/"';
+      requestBody += '"type": "Mapping", ';
+      requestBody += '"map_type": "Same As", ';
+      requestBody += '"from_concept_url": "' + ORIGIN_SOURCE_URL + range.getCell(row, propertyToPosition[CONCEPT_ID]).getValue() + '/", ';
+      requestBody += '"to_concept_url": "' + CEIL_SOURCE_URL + range.getCell(row, propertyToPosition[CIEL_ID]).getValue() + '/", ';
+      requestBody += '"owner": "' + owner +'", ';
+      requestBody += '"owner_type": "' + ownerType +'", ';
+      requestBody += '"source": "' + source +'"';
       requestBody += '}\n';
     }
 
     if (range.getCell(row, propertyToPosition[ICD10_ID]).getValue()) {
       //ICD 10
       requestBody += '{ ';
-      requestBody += 'type: "Mapping", ';
-      requestBody += 'map_type: "Same As", ';
-      requestBody += 'from_concept_url: "' + ORIGIN_SOURCE_URL + range.getCell(row, propertyToPosition[CONCEPT_ID]).getValue() + '/", ';
-      requestBody += 'to_concept_url: "' + ICD10_SOURCE_URL + range.getCell(row,propertyToPosition[ICD10_ID]).getValue() + '/"';
+      requestBody += '"type": "Mapping", ';
+      requestBody += '"map_type": "Same As", ';
+      requestBody += '"from_concept_url": "' + ORIGIN_SOURCE_URL + range.getCell(row, propertyToPosition[CONCEPT_ID]).getValue() + '/", ';
+      requestBody += '"to_concept_url": "' + ICD10_SOURCE_URL + range.getCell(row,propertyToPosition[ICD10_ID]).getValue() + '/", ';
+      requestBody += '"owner": "' + owner +'", ';
+      requestBody += '"owner_type": "' + ownerType +'", ';
+      requestBody += '"source": "' + source +'" ';
       requestBody += '}\n';
-
     }
 
-    if (range.getCell(row, propertyToPosition[SNOMED_CT_ID]).getValue()) {
+    if (range.getCell(row, propertyToPosition[SNOMED_ID]).getValue()) {
       //SNOMED
       requestBody += '{ ';
-      requestBody += 'type: "Mapping", ';
-      requestBody += 'map_type: "Same As", ';
-      requestBody += 'from_concept_url: "' + ORIGIN_SOURCE_URL + range.getCell(row, propertyToPosition[CONCEPT_ID]).getValue() + '/", ';
-      requestBody += 'to_concept_url: "' + SNOMED_CT_SOURCE_URL + range.getCell(row, propertyToPosition[SNOMED_CT_ID]).getValue() + '/"';
+      requestBody += '"type": "Mapping", ';
+      requestBody += '"map_type": "Same As", ';
+      requestBody += '"from_concept_url": "' + ORIGIN_SOURCE_URL + range.getCell(row, propertyToPosition[CONCEPT_ID]).getValue() + '/", ';
+      requestBody += '"to_concept_url": "' + SNOMED_SOURCE_URL + range.getCell(row, propertyToPosition[SNOMED_ID]).getValue() + '/", ';
+      requestBody += '"owner": "' + owner +'", ';
+      requestBody += '"owner_type": "' + ownerType +'", ';
+      requestBody += '"source": "' + source +'" ';
       requestBody += '}\n';
-
     }
   }
 
@@ -153,11 +169,16 @@ function uploadToOCL() {
 
   var options = {
     'method' : 'post',
-    'contentType': 'application/json',
+    'contentType': 'application/jsonl',
+    'headers': { 'Authorization': 'Token ' },
     'payload' : requestBody
   };
   var response = UrlFetchApp.fetch(OCL_API_ENDPOINT + '/manage/bulkimport/', options);
 
-  ui.alert(response.getContentText("UTF-8"));
+  if (JSON.parse(response.getContentText("UTF-8")).state === "PENDING") {
+    ui.alert("Data has been sent to OCL for processing.");
+  } else {
+    ui.alert("An error occured, please retry uploading.");
+  }
 
 }
